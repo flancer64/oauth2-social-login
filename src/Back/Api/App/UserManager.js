df4df3fa776d51750663540703408ad40b7fcea9
Alex Gusev <alex@flancer64.com>
2025-03-10 11:38:00 +0200

    SVTR-224 Add a subscription date to the user's profile

--- src/Back/Api/App/UserManager.js
+++ src/Back/Api/App/UserManager.js
@@ -7,17 +7,6 @@
  */
 export default class Fl64_OAuth2_Social_Back_Api_App_UserManager {
     /**
-     * Finds a user in the application's database by criteria.
-     * @param {Object} params
-     * @param {TeqFw_Db_Back_RDb_ITrans} [params.trx] - The transaction context.
-     * @param {string} [params.email] - The email address of the user.
-     * @param {number} [params.id] - The unique identifier of the user.
-     * @returns {Promise<Object|null>} - The user object if found, or null if not found.
-     * @deprecated we have a `/fl64/oauth/social/user/identity` table locally
-     */
-    async findUser({trx, email, id}) {}
-
-    /**
      * Creates a new user in the application's database.
      * @param {Object} params
      * @param {TeqFw_Db_Back_RDb_ITrans} [params.trx] - The transaction context.
@@ -26,13 +15,4 @@ export default class Fl64_OAuth2_Social_Back_Api_App_UserManager {
      * @returns {Promise<{id: number}>} - The unique identifier of the created user.
      */
     async createUser({trx, identity, extras}) {}
-
-    /**
-     * Updates an existing user's information in the application's database.
-     * @param {Object} params
-     * @param {number} params.id - The unique identifier of the user.
-     * @param {Object} [params.extras] - Additional attributes to update (e.g., name, avatar).
-     * @returns {Promise<Object>} - The updated user object.
-     */
-    async updateUser({trx, id, extras}) {}
 }
