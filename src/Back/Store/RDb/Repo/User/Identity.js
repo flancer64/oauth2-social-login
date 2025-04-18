f79d483814583f0f5f6ec26609d7857836cfa5d8
Alex Gusev <alex@flancer64.com>
2025-04-18 16:38:17 +0300

    SVTR-287 Add an account closing use case

--- src/Back/Store/RDb/Repo/User/Identity.js
+++ src/Back/Store/RDb/Repo/User/Identity.js
@@ -57,13 +57,14 @@ export default class Fl64_OAuth2_Social_Back_Store_RDb_Repo_User_Identity {
         /**
          * @param {Object} params
          * @param {TeqFw_Db_Back_RDb_ITrans} [params.trx]
-         * @param {Object} params.conditions
+         * @param {TeqFw_Db_Shared_Dto_List_Selection.Dto} [params.selection]
+         * @param {Object} [params.conditions]
          * @param {Object<string, 'asc'|'desc'>} [params.sorting]
          * @param {{limit: number, offset: number}} [params.pagination]
          * @returns {Promise<{records: Array<Fl64_OAuth2_Social_Back_Store_RDb_Schema_User_Identity.Dto>}>}
          */
-        this.readMany = async function ({trx, conditions, sorting, pagination}) {
-            return crud.readMany({schema, trx, conditions, sorting, pagination});
+        this.readMany = async function ({trx, selection, conditions, sorting, pagination}) {
+            return crud.readMany({schema, trx, selection, conditions, sorting, pagination});
         };
 
         /**
